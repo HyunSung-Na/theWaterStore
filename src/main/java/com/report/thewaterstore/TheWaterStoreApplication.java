ef2729d60c5ce2dc46690dedf6b24baa21481e6a
HyunSung-Na <oceana57@naver.com>
2021-05-28 11:40:41 +0900

    project setting

---
+++ src/main/java/com/report/thewaterstore/TheWaterStoreApplication.java
@@ -0,0 +1,13 @@
+package com.report.thewaterstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheWaterStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheWaterStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/report/thewaterstore/TheWaterStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.report.thewaterstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheWaterStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
